c8611c1a1accdea01b7156a02eb7dc44cbead1d9
amit kumar <71318008+amitkumar70512@users.noreply.github.com>
2022-07-11 12:23:46 +0530

    Add files via upload

---
+++ bda lab test 2/Amitkumar/src/sqReducer.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+public class sqReducer extends MapReduceBase implements
+Reducer<Text,IntWritable, Text, IntWritable> {
+// Reduce function
+public void reduce(Text key, Iterator<IntWritable> value,
+OutputCollector<Text, IntWritable> output,
+Reporter rep) throws IOException
+{
+	
+int sum = 0;
+// Counting the frequency of each words
+	while (value.hasNext())
+	{
+	IntWritable i = value.next();
+	sum += i.get();
+	}
+output.collect(key, new IntWritable(sum));
+}
+}
